941183b1363a76924dd8152ef79c61766a2ec002
Vinicius Mazon <vnpmazon@gmail.com>
2021-04-21 10:13:51 -0300

    feat: fetch api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
-export default function Home() {
-  return (
-    <h1>Index</h1>
-  )
+export default function Home(props) {
+  return <h1>Index</h1>;
+}
+
+export async function getServerSideProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+  };
 }
